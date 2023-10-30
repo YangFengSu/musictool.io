0f4e01869bb82777baf4c3ae6fe9fe32eef00c47
YangFengSu <148070757+YangFengSu@users.noreply.github.com>
2023-10-30 22:14:21 +0800

    Add files via upload
    I mainly use 'Python' and 'ffmpeg' to convert m4a and flac type files to mp3 files, and the executable file exe under folder bin can be used in an environment without python, provided that the user has installed ffmpeg .exe program
    
    我主要使用‘python’和‘ffmpeg’实现将m4a和flac类型文件转换成mp3格式的文件，文件夹bin下的可执行文件exe可以在没有python的环境下使用，前提是用户安装了ffmpeg.exe程序
    下面是ffmpeg使用的必要常识：
    1、ffmpeg使用时要么选择把ffmpeg.exe添加到（运行路径）所在的文件夹中
    当然也可以不添加，但是需要ffmpeg.exe所在文件夹添加到系统环境变量中
    使用例子：（比如）
    有首歌a.flac在文件夹b中，那么需要在文件夹b中复制一份ffmpeg.exe，然后在文件夹所在目录打开cmd，输入ffmpeg指令就可以运行，当然也可以运行有ffmpeg命令的bat脚本。

---
+++ main.py
@@ -0,0 +1,49 @@
+import os
+
+
+def my_input():
+    print('\n---\nPlease enter the destination path.\n请输入待转换的歌曲文件的路径，直接点一下路径栏，把地址复制过来就可以\n地址示例:E:\\最近常用\\temp\\m4a文件夹')
+    return input()
+
+
+if __name__ == '__main__':
+    path = my_input()
+    while 1:
+        try:
+            last_list = []
+            for i in os.listdir(path):
+                try:
+                    if i.split('.')[-1] == 'm4a':
+                        last_list.append('ffmpeg -i {} -acodec libmp3lame -q:a 2 \"mp3\\{}.mp3\"'.format(i, i.split('.')[0]))
+                    elif i.split('.')[-1] == 'flac':
+                        last_list.append('ffmpeg -i \"{}\" -q:a 0 -map a \"mp3\\{}.mp3\"'.format(i, i.split('.')[0]))
+                    else:
+                        continue  # 当然可以添加其他文件类型，这里暂时不添加，因为主流就是m4a和flac
+                except IndexError:
+                    continue  # 可能不可分，比如文件夹，那么不管它
+
+            if len(last_list) == 0:
+                input("There is no song. Enter any key to continue\nfail.\n没有歌曲\n按任意键继续")
+                path = my_input()
+                continue
+
+            save_file_path = path + '\\{}2mp3.bat'.format('music')
+            with open(save_file_path, 'w', encoding='ANSI') as f:
+                if not os.path.exists(path + '\\mp3'):
+                    os.mkdir(path + '\\mp3')
+                for i in last_list:
+                    print(i)
+                    f.write(i)
+                    f.write('\n\n')
+                f.close()
+            break_num = input("\nsucceed.\tEnter any key (not null) to continue, or press enter to exit\n请到m4a(或者flac)文件夹中运行bat脚本即可！\n输入任意【非空值】继续，直接回车则退出")
+            if break_num == '':
+                break
+            else:
+                path = my_input()
+                continue
+
+        except FileNotFoundError:
+            print('-----\nerror folder.\n系统找不到指定的路径,请检查你输入的路径是否有误\n')
+            print('请输入待转换的歌曲文件的路径，直接点一下路径栏，把地址复制过来就可以\n地址示例:E:\\最近常用\\temp\\m4a文件夹')
+            path = input()
